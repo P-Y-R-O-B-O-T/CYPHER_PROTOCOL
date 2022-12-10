fa9f977493f8ed9c3ade1ca69a6e636aae6ecd40
KUSHAL <KUSHAL@KUSHAL.com>
2022-12-10 21:21:15 +0530

    implemented FTP using CYPHER_PROTOCOL

---
+++ CYPHER_PROTOCOL/CYPHER_CLIENT/FTP/ftp_client.py
@@ -0,0 +1,135 @@
+import threading
+import time
+import random
+import os
+import mimetypes
+from ..cypher_client import CYPHER_CLIENT
+
+#$$$$$$$$$$#
+
+class FTP_CLIENT(CYPHER_CLIENT) :
+    def __init__(self,
+                 file_responce_trigger: object,
+                 recv_chunk_size: int = 1024*1024*1,
+                 transmission_chunk_size: int = 1024*1024*1,
+                 **init_data) -> None :
+
+        super().__init__(responce_handler=self.responce_processor, **init_data)
+
+        self.RECV_CHUNK_SIZE = recv_chunk_size
+        self.TRANSMISSION_CHUNK_SIZE = transmission_chunk_size
+
+        self.FIRST_FETCH = True
+        self.CHAR_POSITION = 0
+        self.FILES_TO_FETCH = []
+
+        self.DOWNLOAD_PATH = ""
+
+        self.TRIGGER = file_responce_trigger
+
+    def responce_processor(self,
+                           responce: dict) -> None :
+        if responce["OPERATION"] == "READ" :
+            if type(responce["DATA"]) is list : self.FILES_TO_FETCH = responce["DATA"]
+            else :
+                self.process_file_data(responce)
+                if self.TRIGGER != None : self.TRIGGER("READ",
+                                                       event_info={"PATH": responce["PATH"],
+                                                                   "METADATA": responce["METADATA"]})
+        elif responce["OPERATION"] == "WRITE" :
+            if self.TRIGGER != None : self.TRIGGER("WRITE", event_info=responce)
+        elif responce["OPERATION"] == "LISTITEMS" :
+            if self.TRIGGER != None : self.TRIGGER("LISTITEMS", event_info=responce)
+        elif responce["OPERATION"] == "DELETE" :
+            if self.TRIGGER != None : self.TRIGGER("DELETE", event_info=responce)
+
+    def process_file_data(self,
+                          responce: dict) -> None :
+        if os.path.isfile(os.path.join(self.DOWNLOAD_PATH, responce["PATH"])) :
+            if (responce["DATA"] != "") or (responce["DATA"] != "b''") :
+                self.write_to_file(responce)
+                self.CHAR_POSITION += self.RECV_CHUNK_SIZE
+            if (responce["DATA"] == "") or (responce["DATA"] == "b''") :
+                self.FILES_TO_FETCH.pop(0)
+                self.CHAR_POSITION = 0
+
+    def create_directory(self) -> None :
+        for _ in self.FILES_TO_FETCH :
+            if not os.path.isfile(os.path.join(self.DOWNLOAD_PATH, _)) :
+                dir_file_sep_position = 0
+                _ = "./"+_
+                for __ in range(len(_)-1, -1,-1) :
+                    if _[__] == "/" :
+                        dir_file_sep_position = __
+                        break
+                if not os.path.isdir(os.path.join(self.DOWNLOAD_PATH, _[:dir_file_sep_position])) :
+                    print(_[:dir_file_sep_position])
+                    print(os.path.join(self.DOWNLOAD_PATH, _[:dir_file_sep_position]))
+                    os.makedirs(os.path.join(self.DOWNLOAD_PATH, _[:dir_file_sep_position]))
+            self.create_file(_)
+
+    def create_file(self,
+                    file: str) -> None :
+        file_obj = open(os.path.join(self.DOWNLOAD_PATH, file), "wb")
+        try :
+            file_obj.write(b'')
+            file_obj.flush()
+        except : pass # self.FILES_TO_FETCH = []
+        finally : file_obj.close()
+
+    def write_to_file(self,
+                      responce: dict) -> None :
+        file_obj = open(os.path.join(self.DOWNLOAD_PATH, responce["PATH"]), "ab")
+        try :
+            file_obj.write(b''.fromhex(responce["DATA"]))
+            file_obj.flush()
+        except : pass # self.FILES_TO_FETCH = []
+        finally : file_obj.close()
+
+    def fetch_file_s(self,
+                     path: str,
+                     download_path: str) -> None :
+        self.DOWNLOAD_PATH = download_path
+        self.make_request(path=path, operation="READ")
+        self.create_directory()
+
+        while self.FILES_TO_FETCH != [] :
+            self.make_request(path=self.FILES_TO_FETCH[0],
+                              operation="READ",
+                              data=self.CHAR_POSITION)
+
+    def upload_file_s(self,
+                      path: str,
+                      server_path: str) -> None :
+        dir_list = []
+        print(path)
+        if os.path.isdir(path) :
+            for addrs, dirs, files in os.walk(path) :
+                for file in files :
+                    dir_list.append(os.path.join(addrs, file))
+        elif os.path.isfile(path) : dir_list.append(path)
+        print(dir_list)
+
+        for _ in dir_list :
+            file_obj = open(os.path.join("", _), "rb")
+            file_data = [file_obj.read(self.TRANSMISSION_CHUNK_SIZE).hex(), 0]
+            while file_data[0] != "" :
+                try : file_size = os.path.getsize(_)
+                except : break
+
+                self.make_request(path=os.path.join(server_path, _),
+                                  operation="WRITE", data=file_data,
+                                  metadata={"FILESIZE": file_size,
+                                            "UPLOADED": file_obj.tell()})
+                file_data[1] += 1
+                try : file_data[0] = file_obj.read(self.TRANSMISSION_CHUNK_SIZE).hex()
+                except : file_data[0] = ""
+            file_obj.close()
+
+    def get_dir_content(self,
+                        path: str) -> None :
+        self.make_request(path=path, operation="LISTITEMS")
+
+    def delete_file_folder(self,
+                           path: str) -> None :
+        self.make_request(path=path, operation="DELETE")
---
+++ CYPHER_PROTOCOL/CYPHER_SERVER/FTP/ftp_server.py
@@ -0,0 +1,135 @@
+from ..cypher_server import CYPHER_SERVER
+import os
+import threading
+import sys
+
+#$$$$$$$$$$#
+
+class FTP_SERVER(CYPHER_SERVER) :
+    def __init__(self, path: str,
+                 recv_chunk_size: int = 1024*1024*1,
+                 transmission_chunk_size: int = 1024*1024*1,
+                 **init_data) -> None :
+
+        super().__init__(request_handler=self.file_data, **init_data)
+
+        self.RECV_CHUNK_SIZE = recv_chunk_size
+        self.TRANSMISSION_CHUNK_SIZE = transmission_chunk_size
+
+        self.PATH = path
+
+        while True :
+            if self.PATH[-1] == "/" : self.PATH = self.PATH[:-1]
+            else : break
+            
+        if not os.path.isdir(self.PATH) :
+            try : os.makedirs(self.PATH)
+            except Exception as EXCEPTION :
+                print(EXCEPTION)
+                sys.exit()
+
+    def file_data(self,
+                  request: dict,
+                  ip_port: tuple) -> dict :
+        responce = {"PATH": request["PATH"],
+                    "DATA": "DONE",
+                    "OPERATION": request["OPERATION"],
+                    "METADATA": {}}
+
+        if ".." in request["PATH"] : return responce
+
+        if request["OPERATION"] == "READ" : self.process_read_request(request, responce)
+        elif request["OPERATION"] == "WRITE" : self.handle_write_request(request, responce)
+        elif request["OPERATION"] == "LISTITEMS" : self.handle_listitem_reuqest(request, responce)
+        elif request["OPERATION"] == "DELETE" : self.handle_delete_request(request, responce)
+        else : request["DATA"] == "NONE"
+
+        return responce
+
+    def process_read_request(self,
+                             request: dict,
+                             responce: dict) -> None :
+        if os.path.isfile(os.path.join(self.PATH, request["PATH"])) :
+            if type(request["DATA"]) is int :
+                try :
+                    file_obj = open(os.path.join(self.PATH, request["PATH"]), "rb")
+                    file_obj.seek(request["DATA"])
+                    responce["DATA"] = file_obj.read(self.TRANSMISSION_CHUNK_SIZE).hex()
+                    responce["METADATA"] = {"FILESIZE": os.path.getsize(os.path.join(self.PATH,
+                                                                                     request["PATH"])),
+                                            "DOWNLOADED": file_obj.tell()}
+                except :
+                    responce["DATA"] = ""
+                    responce["METADATA"] = {"FILESIZE": 1, "DOWNLOADED": 1}
+                finally :
+                    try : file_obj.close()
+                    except : pass
+            else :
+                dir_list = []
+                dir_list.append(request["PATH"])
+                responce["DATA"] = dir_list
+        elif os.path.isdir(os.path.join(self.PATH, request["PATH"])) :
+            dir_list = []
+            for addrs, dirs, files in os.walk(os.path.join(self.PATH, request["PATH"])) :
+                for file in files :
+                    dir_list.append(os.path.join(addrs, file)[len(self.PATH)+1:])
+            responce["DATA"] = dir_list
+        else : responce["DATA"] = "NONE"
+
+    def handle_write_request(self,
+                             request: dict,
+                             responce: dict) -> None :
+        if request["DATA"][1] == 0 :
+            try : self.create_directory(request["PATH"])
+            except : responce["DATA"] = "NONE"
+        try : self.write_to_file(request)
+        except : responce["DATA"] = "NONE"
+
+    def handle_listitem_reuqest(self,
+                                request: dict,
+                                responce: dict) -> None :
+        try : responce["DATA"] = os.listdir(os.path.join(self.PATH, request["PATH"]))
+        except : responce["DATA"] = None
+
+    def handle_delete_request(self,
+                              request: dict,
+                              responce: dict) -> None :
+        if os.path.isdir(os.path.join(self.PATH, request["PATH"])) :
+            os.system("rm -r "+os.path.join(self.PATH, request["PATH"]))
+        elif os.path.isfile(os.path.join(self.PATH, request["PATH"])) :
+            os.system("rm "+os.path.join(self.PATH, request["PATH"]))
+        else : responce["DATA"] = "DO NOT EXIST"
+
+    def create_directory(self,
+                         file: str) -> None :
+        if not os.path.isfile(os.path.join(self.PATH, file)) :
+            dir_file_sep_position = 0
+            for _ in range(len(file)-1, -1, -1) :
+                if file[_] == "/" :
+                    dir_file_sep_position = _
+                    break
+            if not os.path.isdir(os.path.join(self.PATH, file[:dir_file_sep_position])) :
+                os.makedirs(os.path.join(self.PATH, file[:dir_file_sep_position]))
+        self.create_file(file)
+
+    def create_file(self,
+                    file: str) -> None :
+        file_obj = open(os.path.join(self.PATH, file), "wb")
+        try :
+            file_obj.write(b'')
+            file_obj.flush()
+        except : pass
+        finally :
+            try : file_obj.close()
+            except : pass
+
+    def write_to_file(self,
+                      request: dict) -> None :
+        file_obj = open(os.path.join(self.PATH, request["PATH"]), "ab")
+        try :
+            file_obj.write(b''.fromhex(request["DATA"][0]))
+            file_obj.flush()
+        except : pass
+        finally :
+            try : file_obj.close()
+            except : pass
